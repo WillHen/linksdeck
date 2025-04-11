8cc2542a1062455d6bbcf81629f418d0031de055
William H <whenshaw87@googlemail.com>
2025-04-10 22:41:36 -0500

    remove config options

--- tests/change-email.spec.ts
+++ tests/change-email.spec.ts
@@ -1,8 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.configure({ mode: 'serial' });
-
-test.use({ storageState: 'playwright/.auth/user.json' });
 
 test.describe('Change Email', () => {
     test.beforeEach(async ({ page }) => {
