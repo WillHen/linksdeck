fc90be2f0644380fc49621bb40618867482be884
William H <whenshaw87@googlemail.com>
2025-05-08 22:17:09 -0500

    add more users for playwright test (#7)

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,4 +1,14 @@
 import { test, expect } from '@playwright/test';
+import path from 'path';
+
+const authFilePath = (user: string) =>
+  path.join(__dirname, `../playwright/.auth/${user}.json`);
+
+
+test.use({
+  storageState: authFilePath('user1'), // Load user1's authentication state
+});
+
 
 test('test', async ({ page }) => {
   await page.goto('localhost:3000');
