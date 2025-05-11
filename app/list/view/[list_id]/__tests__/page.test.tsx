a0e482baaacf1e349cdc396461f20a897a5396b3
William H <whenshaw87@googlemail.com>
2025-05-11 17:44:48 -0500

    refactor view test to use MSW

--- app/list/view/[list_id]/__tests__/page.test.tsx
+++ app/list/view/[list_id]/__tests__/page.test.tsx
@@ -2,71 +2,30 @@ import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import Page from '../page';
 
-// Mock the underlying functions
-jest.mock('@/app/utils', () => ({
-  getListsFromSupabaseAnon: jest.fn(),
-  getLinksFromSupabaseAnon: jest.fn()
+jest.mock('next/headers', () => ({
+  cookies: jest.fn(() => ({
+    get: jest.fn((key) => {
+      if (key === 'auth-token') return { value: 'mock-token' };
+      return null;
+    }),
+    getAll: jest.fn(() => [{ name: 'auth-token', value: 'mock-token' }])
+  }))
 }));
-
-jest.mock('@/utils/supabase/server', () => ({
-  createClient: jest.fn()
-}));
-
-import {
-  getListsFromSupabaseAnon,
-  getLinksFromSupabaseAnon
-} from '@/app/utils';
-
-const listData = { title: 'Test Title', description: 'Test Description' };
-const linksData = [
-  {
-    id: '1',
-    title: 'Link 1',
-    description: 'Description 1',
-    url: 'http://example.com/1'
-  },
-  {
-    id: '2',
-    title: 'Link 2',
-    description: 'Description 2',
-    url: 'http://example.com/2'
-  }
-];
 
 describe('ViewListPage', () => {
-  beforeAll(() => {
-    (getListsFromSupabaseAnon as jest.Mock).mockReturnValue({
-      eq: () => ({ single: () => ({ data: listData, error: null }) })
-    });
-    (getLinksFromSupabaseAnon as jest.Mock).mockReturnValue({
-      eq: () => ({ data: linksData, error: null })
-    });
-  });
-
-  afterAll(() => {
-    jest.resetAllMocks();
-  });
-
   it('renders the list details and links', async () => {
-    const Result = await Page({ params: { list_id: '1' } });
+    const Result = await Page({
+      params: Promise.resolve({
+        list_id: '6380887d-8749-4f6f-8380-821108847402'
+      })
+    });
     render(Result);
 
-    // Wait for the list title to be rendered
     await waitFor(() =>
-      expect(screen.getByText('Test Title')).toBeInTheDocument()
+      expect(screen.getByText('My List')).toBeInTheDocument()
     );
 
-    // Assert list details
-    expect(screen.getByText('Test Title')).toBeInTheDocument();
-    expect(screen.getByText('Test Description')).toBeInTheDocument();
-
-    // Assert links
-    linksData.forEach((link) => {
-      expect(screen.getByText(link.title)).toBeInTheDocument();
-      expect(screen.getByText(link.title).closest('a')).toHaveAttribute(
-        'href',
-        link.url
-      );
-    });
+    expect(screen.getByText('Example Link 2')).toBeInTheDocument();
+    expect(screen.getByText('https://example.com')).toBeInTheDocument();
   });
 });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,9 @@
 import { http, HttpResponse } from 'msw';
 
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env.test', override: true });
+
 export const handlers = [
     http.get('api/user', () => {
         return HttpResponse.json({ user: { id: '123', name: 'Test User' } })
@@ -20,5 +24,33 @@ export const handlers = [
 
     http.put('api/lists/:list_id', () => {
         return HttpResponse.json({ ok: true })
+    }),
+
+    http.get(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/lists?select=*&list_id=eq.1`, () => {
+        return HttpResponse.json(
+            {
+                id: '6380887d-8749-4f6f-8380-821108847402',
+                user_id: '85dbe305-9eb3-4e91-b9c2-fe9fa10c23c2',
+                title: 'My List',
+                description: 'This is a test list',
+                public: true,
+                created_at: '2025-05-10T15:20:02.431885',
+                link_count: 1
+
+            })
+    }),
+
+    http.get(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/links?select=*&list_id=eq.6380887d-8749-4f6f-8380-821108847402`, () => {
+        return HttpResponse.json([
+            {
+                id: '25de3ed8-12d3-485f-8a72-ce71f4e92ad7',
+                list_id: '6380887d-8749-4f6f-8380-821108847402',
+                url: 'https://example.com',
+                title: 'Example Link 2',
+                description: 'This is an example link',
+                created_at: '2025-05-10T15:20:02.436091',
+                user_id: '85dbe305-9eb3-4e91-b9c2-fe9fa10c23c2'
+            }
+        ])
     })
 ];
